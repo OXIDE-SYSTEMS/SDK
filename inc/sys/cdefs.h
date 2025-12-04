300c6b187ae06e460801ff5155e95b67e98d2b65
Ian Moffett <ian@osmora.org>
2025-12-04 17:14:19 -0500

    sys: Add sys/cdefs.h
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ inc/sys/cdefs.h
@@ -0,0 +1,19 @@
+/*
+ * -------------------------------------------------------------
+ * OXIDE system type defines
+ * Copyright (c) 2025, Ian Moffett and the OXIDE SYSTEMS engineers
+ * All rights reserved.
+ * Provided under the BSD 3-Clause license.
+ * -------------------------------------------------------------
+ */
+
+#ifndef _SYS_CDEFS_H_
+#define _SYS_CDEFS_H_ 1
+
+#define ATTR(x)         __attribute__((x))
+#define PACKED          ATTR(packed)
+#define NORETURN        ATTR(noreturn)
+#define ALIGN(n)        ATTR(aligned(n))
+#define ALWAYS_INLINE   ATTR(always_inline)
+
+#endif  /* !_SYS_CDEFS_H_ */
