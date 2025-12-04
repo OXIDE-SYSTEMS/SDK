275ff1e487833b22ba2bac73ecb7ead0b40962e1
Ian Moffett <ian@osmora.org>
2025-12-04 17:16:57 -0500

    sys: cdefs: Add inline assembly wrapper
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- inc/sys/cdefs.h
+++ inc/sys/cdefs.h
@@ -10,6 +10,7 @@
 #ifndef _SYS_CDEFS_H_
 #define _SYS_CDEFS_H_ 1
 
+#define ASMV            __asm__ __volatile__
 #define ATTR(x)         __attribute__((x))
 #define PACKED          ATTR(packed)
 #define NORETURN        ATTR(noreturn)
