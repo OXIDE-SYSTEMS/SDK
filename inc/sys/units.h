b0f3145dc2dc7c9f43e62db05b6b8e8621f65b00
Ian Moffett <ian@osmora.org>
2025-12-04 22:41:34 -0500

    sys: Add sys/units.h
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ inc/sys/units.h
@@ -0,0 +1,20 @@
+/*
+ * -------------------------------------------------------------
+ * OXIDE standard unit defines
+ * Copyright (c) 2025, Ian Moffett and the OXIDE SYSTEMS engineers
+ * All rights reserved.
+ * Provided under the BSD 3-Clause license.
+ * -------------------------------------------------------------
+ */
+
+#ifndef _SYS_UNITS_H_
+#define _SYS_UNITS_H_ 1
+
+/* Frequency units */
+#define UNIT_MHZ 1000000        /* Hz */
+
+/* Storage size units */
+#define UNIT_GIB 0x40000000
+#define UNIT_MIB 0x100000
+
+#endif  /* !_SYS_UNITS_H_ */
